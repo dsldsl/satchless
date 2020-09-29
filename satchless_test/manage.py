3bed3db0f3bfa8e10105824ba94a09e09dc88c9a
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-29 14:54:32 -0500

    Try other method of collecting satchless tests

--- satchless_test/manage.py
+++ satchless_test/manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # Put satchless in import path
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "satchless_test.settings")
     try:
         from django.core.management import execute_from_command_line
