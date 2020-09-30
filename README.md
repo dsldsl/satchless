4d6781f35070d4bb3f84891a03dd3d7e701d3948
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-29 19:13:46 -0500

    Coveralls support

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Coverage Status](https://coveralls.io/repos/github/styleseat/satchless/badge.svg?branch=master)](https://coveralls.io/github/styleseat/satchless?branch=master)
+
+## satchless (StyleSeat Fork)
+
+This is a heavily modified fork of an old version satchless.
--- run-tests.sh
+++ run-tests.sh
@@ -4,11 +4,16 @@ set -uexo pipefail
 
 cd $(dirname $0)
 
-
 # Discover satchless apps which contain tests
 apps_with_test_dirs=( $(find . | grep 'tests/__init__.py$' | sed 's/\/tests\/__init__\.py//g' | sed 's/\//./g' | sed 's/^\.\.//g') )
 apps_with_test_py=( $(find . | grep 'test[a-z_0-9].py$' | sed 's/\/test.*py//g' | sed 's/\//./g' | sed 's/^\.\.//g') )
 apps_to_test=( "${apps_with_test_dirs[@]}" "${apps_with_test_py[@]}" )
 
 echo "Running tests for ${apps_to_test[@]}"
-exec python satchless_test/manage.py test ${apps_to_test[@]}
+if [[ ! -z ${COVERAGE:-} ]]
+then
+  prefix="coverage run --source=satchless"
+else
+  prefix="python"
+fi
+exec $prefix satchless_test/manage.py test ${apps_to_test[@]}
