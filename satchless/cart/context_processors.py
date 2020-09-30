28453ecd9a12261c2c032dc0b28f229fcf163a00
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-29 19:42:46 -0500

    Remove template tags

--- satchless/cart/context_processors.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from . import models
-
-def satchless_cart(request):
-    return {'satchless_cart': (models.Cart.objects.get_or_create_from_request(request, 'cart'))}
