0b4b276e564ae75b03d5296fae13e0710f62f82b
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-30 22:05:26 -0500

    Remove unused code

--- satchless/util/models.py
+++ satchless/util/models.py
@@ -1,49 +1,14 @@
 from __future__ import absolute_import
 from django.db import models
-from django.db.models.fields.related_descriptors import ReverseOneToOneDescriptor
 from django.dispatch import receiver
-
-class SubtypedManager(models.Manager):
-    def find_subclasses(self, root):
-        for a in dir(root):
-            attr = getattr(root, a)
-            if isinstance(attr, ReverseOneToOneDescriptor):
-                child = attr.related.model
-                if (issubclass(child, root) and
-                    child is not root):
-                    yield a
-                    for s in self.find_subclasses(child):
-                        yield '%s__%s' % (a, s)
-
-    # https://code.djangoproject.com/ticket/16572
-    #def get_queryset(self):
-    #    qs = super(SubtypedManager, self).get_queryset()
-    #    subclasses = list(self.find_subclasses(self.model))
-    #    if subclasses:
-    #        return qs.select_related(*subclasses)
-    #    return qs
 
 
 class Subtyped(models.Model):
     subtype_attr = models.CharField(max_length=500, editable=False)
     __in_unicode = False
 
-    objects = SubtypedManager()
-
     class Meta:
         abstract = True
-
-    def __str__(self):
-        # XXX: can we do it in more clean way?
-        if self.__in_unicode:
-            return super(Subtyped, self).__unicode__()
-        elif type(self.get_subtype_instance()) == type(self):
-            self.__in_unicode = True
-            res = self.__unicode__()
-            self.__in_unicode = False
-            return res
-        else:
-            return self.get_subtype_instance().__unicode__()
 
     def get_subtype_instance(self):
         """
