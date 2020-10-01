f7ef7995d7f02527e8ec6d1ed6e21be5cdc7a615
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-10-01 14:16:07 -0500

    not-secret

--- satchless_test/satchless_test/settings.py
+++ satchless_test/satchless_test/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lzi@ew!)-trzb!#2y+$7p)h4med75!fy$&p70bbff$lcmhryiz'
+SECRET_KEY = 'not-secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
